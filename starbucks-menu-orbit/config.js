9ce72dd2320330243577c5de511020995edb1898
dariakimberly4-netizen <dariakimberly4@gmail.com>
2026-08-29 12:20:17 +0800

    Connect Menu Orbit to pickup ordering backend

---
+++ starbucks-menu-orbit/config.js
@@ -0,0 +1,5 @@
+window.MENU_ORBIT_CONFIG = {
+  supabaseUrl: "https://hknjlixpgznqqxxjffwi.supabase.co",
+  supabasePublishableKey: "sb_publishable_6tKxtD2AR4oErplieVyrtQ_jjKytRrY",
+  branches: ["Demo Pickup Branch"]
+};
