fa6d4bec37d43afd88d7d1d7454acb1d6c97c287
dariakimberly4-netizen <dariakimberly4@gmail.com>
2026-09-02 21:59:00 +0800

    Remove unverified pickup branch configuration

--- starbucks-menu-orbit/config.js
+++ starbucks-menu-orbit/config.js
@@ -1,5 +1,5 @@
 window.MENU_ORBIT_CONFIG = {
   supabaseUrl: "https://hknjlixpgznqqxxjffwi.supabase.co",
   supabasePublishableKey: "sb_publishable_6tKxtD2AR4oErplieVyrtQ_jjKytRrY",
-  branches: ["Starbucks SM City Bacoor", "Starbucks Molino Boulevard", "Starbucks Vista Mall Daang Hari"]
+  branches: []
 };
